1d0b538d53bc8a8dd1d0cfc6199fcbf2844bf44e
marco picasso <marco.picasso@hotmail.it>
2018-06-30 20:48:16 +0200

    Controller logic for file update and save file in folder

--- Controllers/FileUploadController.cs
+++ Controllers/FileUploadController.cs
@@ -1,24 +1,64 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using ApiDmS.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiDmS.Controllers {
 
-    [Route("api/[controller]")]
-    public class FileUploadController : Controller 
-    {
-        [HttpPost("upload")]
-        public ActionResult Post(Document doc)
-        {
-            
+    [Route ("api/[controller]")]
+    public class FileUploadController : Controller {
+        private IHostingEnvironment _hostingenvironment;
 
-            return Ok(doc);
+        public FileUploadController (IHostingEnvironment hostingenvironment) {
+            _hostingenvironment = hostingenvironment;
         }
-        
+
+        [HttpPost ("upload")]
+        public IActionResult Post (Document doc) {
+
+            string este = Path.GetExtension (doc.FileToUpload.FileName);
+            
+            var newFileName = string.Empty;
+
+            var fileName = string.Empty;
+
+            var files = HttpContext.Request.Form.Files;
+            foreach (var file in files) {
+                
+                if (file.Length > 0) {
+                    
+                    //Getting FileName
+                    fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    //Getting file Extension
+                    var FileExt = Path.GetExtension(fileName);
+
+                    //Assigning Unique Filename (Guid)
+                    var myUniqueFileName = Convert.ToString (Guid.NewGuid ());
+
+                    // concating  FileName + FileExtension
+                    newFileName = myUniqueFileName + FileExt;
+
+                    // Combines two strings into a path.
+                    fileName = Path.Combine (_hostingenvironment.WebRootPath, "upload") + $@"\{newFileName}";
+
+                    //Store path of folder in database
+                    //PathDB = "demoImages/" + newFileName;
+
+                    using (FileStream fs = System.IO.File.Create (fileName)) {
+                        file.CopyTo(fs);
+                        fs.Flush ();
+                    }
+                }
+            }
+
+            return Ok();
+        }
 
     }
 }
